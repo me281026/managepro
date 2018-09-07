f8e16e987b721786d81488f4d7f97d98402b4bc3
wanwei_zhou <wanwei_zhou@crfchina.com>
2018-09-07 16:03:10 +0800

    Initial commit

---
+++ src/main/java/me/manage/pro/managepro/ManageproApplication.java
@@ -0,0 +1,12 @@
+package me.manage.pro.managepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageproApplication.class, args);
+	}
+}
---
+++ src/test/java/me/manage/pro/managepro/ManageproApplicationTests.java
@@ -0,0 +1,16 @@
+package me.manage.pro.managepro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManageproApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
